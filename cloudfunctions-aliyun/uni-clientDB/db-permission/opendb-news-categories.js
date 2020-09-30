85581b65d08b4102f943bb3fdb321467b27c7302
guoshize <2874764982@qq.com>
2020-09-30 17:22:32 +0800

    g

--- cloudfunctions-aliyun/uni-clientDB/db-permission/opendb-news-categories.js
+++
@@ -1,6 +0,0 @@
-const rule = {
-	// 文章类别表仅由管理员维护
-	'.read': true
-}
-
-module.exports = rule
--- cloudfunctions-aliyun/uni-clientDB/db-permission/opendb-news-comments.js
+++
@@ -1,6 +0,0 @@
-const rule = {
-	'.read': true,
-	'.create': 'auth.uid != null && action == "add-comment"'
-}
-
-module.exports = rule
